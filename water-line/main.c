2f79f7d4bee4f295068fe0a7a79d52d047828697
trostleg <galen.trostle@gmail.com>
2017-06-15 17:56:04 -0700

    fix time vars, release motor

--- water-line/main.c
+++ water-line/main.c
@@ -1,38 +1,51 @@
+
+
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 
+unsigned long DAY_1 = 86400000;
+unsigned long DAY_2 = 172800000;
+unsigned long MIN_15 = 900000;
+unsigned long MIN_2 = 120000;   
+unsigned long MIN_1 = 60000;
+
+
+
 unsigned long timer = millis();
-unsigned long timeon = 900000;   //open time in millis
-//15 min = 900000
-//= (gal desired/gpm)*60*1000
-unsigned long howoften = 86400000;    //how often to water in millis
-//86400000 = 1 day 
-//= how often in days * 24*60*60*1000
+unsigned long timeon = MIN_15;   //open time in millis
+unsigned long howoften = DAY_2;    //how often to water in millis
+
 
 void setup() {
   AFMS.begin();
-   myMotor->setSpeed(1);
+  myMotor->setSpeed(1);
   Serial.begin(9600);
 
 }
 
 void loop() {
   Serial.println("millis(), timer, howoften, millis-timer :");Serial.print(millis());Serial.print(" | ");Serial.print(timer);Serial.print(" | ");Serial.print(howoften);Serial.print(" | ");Serial.println(millis() - timer);
+ 
   if (millis() - timer > howoften) { //test if time to water
     Serial.print(millis());Serial.print("  ");
     Serial.println(millis()-timer);
     Serial.print("timer= ");Serial.println(timer);
     
     myMotor->step(180, FORWARD, INTERLEAVE); 
+    myMotor->release();
     Serial.println("on");
     delay(timeon);
     myMotor->step(180, BACKWARD, INTERLEAVE);
+    myMotor->release();
     Serial.println("off");
     timer = millis();
     
     
   }
 }
+
+
+
